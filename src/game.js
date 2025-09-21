e9e71ced027a6dce27422b514005ee28d3df9942
deeppatel-dev <99211241+deeppatel-dev@users.noreply.github.com>
2025-09-21 13:51:08 -0700

    feat(Game) added logic for automatic placement of ships on the computer and player boards.

---
+++ src/game.js
@@ -0,0 +1,33 @@
+import { Gameboard } from "./gameboard";
+import { Player } from "./player";
+import { Ship } from "./ship";
+
+function startGame() {
+  const computer = new Player();
+  const player = new Player();
+  console.log("Starting Game.");
+  const playerBoard = player.board;
+  const computerBoard = computer.board;
+  placeShips(playerBoard);
+  placeShips(computerBoard);
+  console.log(playerBoard.board);
+  console.log(computerBoard.board);
+}
+
+function placeShips(board) {
+  const shipLenghts = [5, 4, 3, 3, 2];
+
+  for (let length of shipLenghts) {
+    let p = false;
+
+    while (!p) {
+      const result = Math.random() < 0.5 ? "horizontal" : "vertical";
+      const x = Math.floor(Math.random() * 10);
+      const y = Math.floor(Math.random() * 10);
+      const ship = new Ship(length);
+      p = board.placeShip(x, y, result, ship);
+    }
+  }
+}
+
+export { startGame };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "./styles.css";
+import { startGame } from "./game";
 
 console.log("Document Loaded");
+startGame();
